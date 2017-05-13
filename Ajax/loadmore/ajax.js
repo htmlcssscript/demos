b637afde597ead3b89abc23cbd81fda11690f759
xuan_yan <3239089064@qq.com>
2017-05-13 22:34:50 +0800

    adf

---
+++ Ajax/loadmore/ajax.js
@@ -0,0 +1,40 @@
+
+function ajax(opts){
+ opts.success = opts.success||function(){}
+ opts.error=opts.error||function(){}
+ opts.type=opts.type||'json'
+ opts.data=opts.data||{}
+ var dataStr = ''
+ for(var key in opts.data){
+     dataStr += key+'='+opts.data[key]+'&'
+ }
+ var xmlHttp=new XMLHttpRequest()
+ xmlHttp.onreadystatechange=function(){
+     if(xmlHttp.readyState===4){
+         if(xmlHttp.status>=200 && (xmlHttp.status<300||xmlHttp.status===304)){
+             if(opts.dataType==="text"){
+                opts.success(xmlHttp.responseText)
+             }
+             if(opts.dataType==="json"){
+               var json = JSON.parse(xmlHttp.responseText)
+                opts.success(json)
+             }
+         }else{ 
+             var statusSend = xmlHttp.status
+             opts.error(statusSend)
+         }
+     }
+  }
+
+  dataStr = dataStr.substr(0, dataStr.length-1)
+  if(opts.type.toLowerCase()==='post'){
+   xmlHttp.open(opts.type,opts.url,true)
+   xmlHttp.setRequestHeader('Context-type','application/x-www-form-urlencoded')
+   xmlHttp.send(dataStr)
+  }
+  if(opts.type.toLowerCase()==='get'){
+   xmlHttp.open(opts.type,opts.url+'?'+dataStr,true)
+   xmlHttp.send()
+  }
+}
+// https://imququ.com/post/four-ways-to-post-data-in-http.html
